48202d9e6ff64bbfdd1fdc321f51ab00cb3a6eb1
Alexander Niki <aalexanderniki@gmail.com>
2023-03-15 00:01:32 +0400

    added information to footer and styled footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -15,23 +15,26 @@ class Footer extends HTMLElement {
     render() {
         this.innerHTML = `
         <div class="uix-layout--hbox--wrapped"  id="footer--container--main">
-                <span>
-                    Контакты:
-                    <ul>
-                        <li>Telegram-канал: <a href="https://t.me/surflguide">@surflguide</a></li>
-                        <li>Telegram-бот: <a href="https://t.me/surflbot">@surflbot</a></li>
-                        <li><a href="mailto:surflguide@gmail.com">surflguide@gmail.com</a></li>
-                    </ul>
-                </span>
-                <span>
-                    Информация:
-                    <ul>
-                        <li><a href="about.html">О проекте</a></li>
-                        <li><a href="contribute.html">Помочь проекту</a></li>
-                    </ul>
-                </span>
-            </div>
-            <span>Made with ❤ in Russia</span>
+            <span>
+                Контакты:
+                <ul>
+                    <li>Telegram-канал: <a href="https://t.me/surflguide">@surflguide</a></li>
+                    <li>Telegram-бот: <a href="https://t.me/surflbot">@surflbot</a></li>
+                    <li><a href="mailto:surflguide@gmail.com">surflguide@gmail.com</a></li>
+                </ul>
+            </span>
+            <span>
+                Информация:
+                <ul>
+                    <li><a href="about.html">О проекте</a></li>
+                    <li><a href="contribute.html">Помочь проекту</a></li>
+                </ul>
+            </span>
+        </div>
+        <div class="uix-layout--vbox" id="footer--container--main--label">
+            <center><span>Made with ❤ in Russia</span><br>
+            <span>Est. 2020</span></center>
+        </div>
         `;
     }
 }
