980a890fb42b4261dabc04e24ec4d52f55228f37
Alexander Niki <aalexanderniki@gmail.com>
2023-03-24 17:29:52 +0400

    renamed some files

--- dist/app/app.js
+++ dist/app/app.js
@@ -257,6 +257,103 @@ class UICard extends HTMLElement {
 
 customElements.define("ui-card", UICard);
 /*
+ * communicationcard.js
+ * UICardCommunication
+ */
+
+
+class UICardCommunication extends UICard {
+
+    constructor() {
+        super();
+
+        this._type = "";
+        this._channelType = "";
+        this._link = "";
+        this._linkText = "";
+    }
+
+    new(model) {
+        this.type = model.type;
+        this.channelType = model.platform;
+        this.primaryText = model.name;
+        this.secondaryText = model.summary;
+        this.link = model.link;
+        this.linkText = model.link_text;
+        return this;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    get channelType() {
+        return this._channelType;
+    }
+
+    get link() {
+        return this._link;
+    }
+
+    get linkText() {
+        return this._linkText;
+    }
+
+    set type(str) {
+        if (str) {
+            this._type = str;
+        }
+        else {
+            //do nothing
+        }
+    }
+
+    set channelType(str) {
+        if (str) {
+            this._channelType = str;
+        }
+        else {
+            // do nothing
+        }
+    }
+
+    set link(str) {
+        if (str) {
+            this._link = str;
+        }
+        else {
+            //do nothing
+        }
+    }
+
+    set linkText(str) {
+        if (str) {
+            this._linkText = str;
+        }
+        else {
+            this.linkText = "Ссылка";
+        }
+    }
+
+    render() {
+        this.innerHTML = `
+            <div class="ui-card--communication">
+                <span class="caption typography-uppercase">${this.type} • ${this.channelType}</span>
+                <span class="caption-accent">${this.primaryText}</span>
+                <span class="body-1"><a href="${this.link}">${this.linkText}</a></span>
+                <span class="body-1">${this.secondaryText}</span>
+            </div>
+        `;
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+}
+
+
+customElements.define("ui-card--communication", UICardCommunication);
+/*
  * cardsimple.js
  * UICardSimple
  * 
@@ -364,103 +461,6 @@ class UICardSimple extends UICard {
 }
 
 customElements.define("ui-card--simple", UICardSimple);
-/*
- * communicationcard.js
- * UICardCommunication
- */
-
-
-class UICardCommunication extends UICard {
-
-    constructor() {
-        super();
-
-        this._type = "";
-        this._channelType = "";
-        this._link = "";
-        this._linkText = "";
-    }
-
-    new(model) {
-        this.type = model.type;
-        this.channelType = model.platform;
-        this.primaryText = model.name;
-        this.secondaryText = model.summary;
-        this.link = model.link;
-        this.linkText = model.link_text;
-        return this;
-    }
-
-    get type() {
-        return this._type;
-    }
-
-    get channelType() {
-        return this._channelType;
-    }
-
-    get link() {
-        return this._link;
-    }
-
-    get linkText() {
-        return this._linkText;
-    }
-
-    set type(str) {
-        if (str) {
-            this._type = str;
-        }
-        else {
-            //do nothing
-        }
-    }
-
-    set channelType(str) {
-        if (str) {
-            this._channelType = str;
-        }
-        else {
-            // do nothing
-        }
-    }
-
-    set link(str) {
-        if (str) {
-            this._link = str;
-        }
-        else {
-            //do nothing
-        }
-    }
-
-    set linkText(str) {
-        if (str) {
-            this._linkText = str;
-        }
-        else {
-            this.linkText = "Ссылка";
-        }
-    }
-
-    render() {
-        this.innerHTML = `
-            <div class="ui-card--communication">
-                <span class="caption typography-uppercase">${this.type} • ${this.channelType}</span>
-                <span class="caption-accent">${this.primaryText}</span>
-                <span class="body-1"><a href="${this.link}">${this.linkText}</a></span>
-                <span class="body-1">${this.secondaryText}</span>
-            </div>
-        `;
-    }
-
-    connectedCallback() {
-        this.render();
-    }
-}
-
-
-customElements.define("ui-card--communication", UICardCommunication);
 /*
  * label.js
  * UILabel
